ace2428b218742cb48c2e1ef6fe324be06106fa9
Daniel Solano <daniel-9122@hotmail.com>
2018-04-25 02:25:11 -0600

    Minor change

---
+++ src/Exceptions/ListException.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exceptions;
+
+/**
+ * @version1.0
+ * @author Daniel Solano
+ */
+public class ListException extends Exception {
+
+    public ListException(String message) {
+        super(message);
+    }
+
+    
+    
+    
+}
